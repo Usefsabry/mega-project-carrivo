cd2eb9832eb7404690e86ad94a21b5da134f77f9
nourbasem211 <nourbasem211@gmail.com>
2025-12-13 13:38:27 +0200

    Fix routing and resolve merge issues

--- src/App.js
+++ src/App.js
@@ -13,14 +13,12 @@ import TestPage2 from "./pages/TestPage2";
 import TestPage3 from "./pages/TestPage3";
 import TestResults from "./pages/TestResults";
 import CareerAssessmentStart from "./pages/CareerAssessmentStart";
-<<<<<<< HEAD
+
 import Mentors from "./pages/Mentors";
 import AboutUs from "./pages/AboutUs";
 import StudentDashboard from "./pages/StudentDashboard";
 import CareerPaths from "./pages/CareerPaths";
-=======
 import CareerHub from "./pages/CareerHub";
->>>>>>> 3d4a2dfa19eaa00adef9fc286b724392af5b4b7c
 
 function App() {
   return (
@@ -31,20 +29,23 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+
             <Route path="/assessment-start" element={<CareerAssessmentStart />} />
+
             <Route path="/test/page1" element={<TestPage1 />} />
             <Route path="/test/page2" element={<TestPage2 />} />
             <Route path="/test/page3" element={<TestPage3 />} />
             <Route path="/test/results" element={<TestResults />} />
+
             <Route path="/mentors" element={<Mentors />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/dashboard" element={<StudentDashboard />} />
             <Route path="/paths" element={<CareerPaths />} />
+            <Route path="/career-hub" element={<CareerHub />} />
           </Routes>
         </Router>
       </ProgressProvider>
     </AuthProvider>
-   
   );
 }
 
